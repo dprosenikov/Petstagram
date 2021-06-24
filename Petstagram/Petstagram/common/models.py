50692fa50d3e66a340ebc420b60da95d9c32e13d
dprosenikov <dprosenikov86@gmail.com>
2021-06-25 02:25:44 +0300

    WS 3

--- Petstagram/Petstagram/common/models.py
+++ Petstagram/Petstagram/common/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+from Petstagram.pets.models import Pet
+
+
+class Comment(models.Model):
+    pet = models.ForeignKey(Pet, on_delete=models.CASCADE)
+    comment = models.TextField()
--- Petstagram/Petstagram/pets/views.py
+++ Petstagram/Petstagram/pets/views.py
@@ -1,25 +1,88 @@
 from django.shortcuts import render, redirect
 
+from Petstagram.common.forms import CommentForm
+from Petstagram.common.models import Comment
+from Petstagram.pets.forms import CreatePetForm
 from Petstagram.pets.models import Pet, Like
 
 
 def list_pets(req):
     all_pets = Pet.objects.all()
     context = {
-        'pets' : all_pets
+        'pets': all_pets
     }
-    return render(req,'pets/pet_list.html', context)
+    return render(req, 'pets/pet_list.html', context)
+
 
 def pet_details(req, pk):
     pet = Pet.objects.get(pk=pk)
     pet.likes_count = pet.like_set.count()
     context = {
-        'pet' : pet
+        'pet': pet,
+        'comment': CommentForm(),
+        'comments': pet.comment_set.all()
     }
-    return render(req,'pets/pet_detail.html', context)
+    return render(req, 'pets/pet_detail.html', context)
+
+def comment_pet(request, pk):
+    pet = Pet.objects.get(pk=pk)
+    form = CommentForm(request.POST)
+    if form.is_valid():
+        comment = Comment(
+            comment=form.cleaned_data['comment'],
+            pet = pet,
+        )
+        comment.save()
+    return redirect('pet details', pet.id)
 
 def like_pet(req, pk):
     pet = Pet.objects.get(pk=pk)
     like = Like(pet=pet)
     like.save()
     return redirect('pet details', pk)
+
+
+def create_pet(request):
+    if request.method == 'POST':
+        pet = CreatePetForm(request.POST)
+        if pet.is_valid():
+            pet.save()
+            return redirect('list pets')
+        else:
+            context = {'pet': pet}
+            return render(request, 'pets/pet_create.html', context)
+    else:
+        pet = CreatePetForm()
+        context = {
+            'pet': pet
+        }
+        return render(request, 'pets/pet_create.html', context)
+
+
+def edit_pet(request, pk):
+    selected_pet = Pet.objects.get(pk=pk)
+    if request.method == 'POST':
+        pet = CreatePetForm(request.POST, instance=selected_pet)
+        if pet.is_valid():
+            pet.save()
+            return redirect('list pets')
+        else:
+            context = {'pet': pet}
+            return render(request, 'pets/pet_edit.html', context)
+    else:
+        pet = CreatePetForm(instance=selected_pet)
+        context = {
+            'pet': pet,
+            'selected_pet': selected_pet
+        }
+        return render(request, 'pets/pet_edit.html', context)
+
+
+def delete_pet(request, pk):
+    selected_pet = Pet.objects.get(pk=pk)
+    if request.method == 'POST':
+        selected_pet.delete()
+        return redirect('list pets')
+    else:
+        context = {'pet': selected_pet}
+        return render(request, 'pets/pet_delete.html', context)
